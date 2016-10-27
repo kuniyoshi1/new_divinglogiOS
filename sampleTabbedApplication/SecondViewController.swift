a5e12aeb37f8d945944ceacc8502d9a4e86f8038
kuniyoshi1 <knuiyosi111@gmail.com>
2016-10-27 10:33:39 +0800

    Initial Commit

---
+++ sampleTabbedApplication/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  sampleTabbedApplication
+//
+//  Created by 國吉イチ on 2016/10/27.
+//  Copyright © 2016年 國吉イチ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sampleTabbedApplicationTests/sampleTabbedApplicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  sampleTabbedApplicationTests.swift
+//  sampleTabbedApplicationTests
+//
+//  Created by 國吉イチ on 2016/10/27.
+//  Copyright © 2016年 國吉イチ. All rights reserved.
+//
+
+import XCTest
+@testable import sampleTabbedApplication
+
+class sampleTabbedApplicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

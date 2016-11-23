1e04b1a43adc7e70f8500e6852c18c9d19eca66e
kuniyoshi1 <knuiyosi111@gmail.com>
2016-11-23 10:34:41 +0800

    add map

--- sampleTabbedApplication/SecondViewController.swift
+++ sampleTabbedApplication/SecondViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import CoreData
+import MapKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITextFieldDelegate {
     
     @IBOutlet weak var titletxt: UITextField!
     @IBOutlet weak var point: UITextField!
@@ -70,6 +71,56 @@ class SecondViewController: UIViewController {
             try viewContext.save()
         } catch {
         }
+    }
+
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //①キーボードを閉じる
+        inputText.resignFirstResponder()
+        
+        //②入力された文字を取り出す
+        let serchKeyword = inputText.text
+        
+        //③入力された文字をデバックエリアに表示
+        print("キーワード\(serchKeyword)")
+        
+        //⑤CLGeocoderインスタンスを取得
+        let geocoder = CLGeocoder()
+        
+        //⑥入力された文字から位置情報を取得
+        geocoder.geocodeAddressString(serchKeyword!, completionHandler: { (placemarks:[CLPlacemark]?, error:Error?) in
+            
+            //⑦位置情報が存在する場合１件目の位置情報をplacemarkに取り出す
+            if let placemark = placemarks?[0]{
+                
+                //⑧位置情報から緯度経度が存在する場合、緯度経度をtargetCoordinateに取り出す
+                if let targetCoordinate = placemark.location?.coordinate{
+                    
+                    //⑨緯度経度をデバイスエリアに表示
+                    print(targetCoordinate)
+                    
+                    //⑩MKPointAnnotationインスタンスを取得し、ピンを生成
+                    let pin = MKPointAnnotation()
+                    
+                    //11　ピンの置く場所に緯度経度を設定
+                    pin.coordinate = targetCoordinate
+                    
+                    //12　ピンのタイトルを設定
+                    pin.title = serchKeyword
+                    
+                    //13　ピンを地図に置く
+                    self.dispMap.addAnnotation(pin)
+                    
+                    //14　緯度経度を中心にして半径500mの範囲を表示
+                    self.dispMap.region = MKCoordinateRegionMakeWithDistance(targetCoordinate, 500.0, 500.0)
+                }
+            }
+        })
+        
+        //④デフォルト動作を行うのでtrueを返す
+        return true
     }
     
     
